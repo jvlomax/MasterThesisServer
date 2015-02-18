43229457efc95083298b6dd400d66d3352c8ff97
Jørn Lomax <northlomax@gmail.com>
2015-02-18 01:48:17 +0100

    logoff commit

--- resources/beacons_view.py
+++ resources/beacons_view.py
@@ -7,9 +7,15 @@ from flask.ext.restful import reqparse
 from sqlalchemy import exc
 
 parser = reqparse.RequestParser()
-parser.add_argument("guardian", type=int, help="The id of a guardian")
+parser.add_argument("beacon_id", type=int, help="The id of the beacon")
+parser.add_argument("guardian_id", type=int, help="The id of a guardian")
 parser.add_argument("location", type=str, help="Human readable location of the beacon")
 parser.add_argument("mac_address", type=str, help="Mac address of the node")
+parser.add_argument("team", type=int, help="The team controlling the beacon")
+
+
+
+
 
 class Beacons_view(restful.Resource):
 
@@ -30,17 +36,23 @@ class Beacons_view(restful.Resource):
 
     def put(self):
         args = parser.parse_args()
-        mac_address = args.get("mac_address")
-        location = args.get("location")
+        print(args)
+        id = args.get("beacon_id")
 
-        try:
-            beacon = Beacon(mac_address, location)
-            db.session.add(beacon)
-            db.session.commit()
-        except exc.SQLAlchemyError as e:
-            raise DatabaseError
+        beacon = Beacon.query.get(id)
+
+        print(args.get("mac_address"))
+
+        beacon.mac_address = args.get("mac_address")
+
+        beacon.location = args.get("location")
+        beacon.team = args.get("team")
+        beacon.guardian = args.get("guardian_id")
+
+
+
         return jsonify({"status": "200",
-                        "message": "beacon created",
+                        "message": "beacon moved",
                         "new_beacon_id": beacon.id})
 
 
@@ -60,18 +72,6 @@ class Beacons_view(restful.Resource):
             raise DatabaseError
 
 
-    def move_beacon(self, id, location):
-        b = Beacon.query.get(id)
-        b.location = location
-        db.session.add(b)
-        db.session.commit()
 
-    def change_guardian(self, guardian_id, id, team):
-        beacon = Beacon.query.get(id)
-        beacon.guardian = guardian_id
-        guardian = Guardian.query.get(guardian_id)
-        beacon.team = guardian.team
-        db.session.add(beacon)
-        db.session.commit()
 
 
